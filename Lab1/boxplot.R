de42e4fe588b87fab930d621f7bbc7b0b0fae2ba
Shan Zhong <zhongshanyes@gmail.com>
2019-09-11 13:43:12 -0400

    Shan

--- Lab1/boxplot.R
+++ Lab1/boxplot.R
@@ -1,8 +1,3 @@
-dat <- read.csv("https://raw.githubusercontent.com/Shanlearning/Stat201/master/Lab1/data.csv")
-dat
 
-boxplot(dat[,c("Random_sample_mean","Judgement_sample_mean")])
-abline(h=5.5686)
 
-boxplot(dat[,c("Random_sample_std_deviation","Judgment_sample_std_deviation")])
-abline(h=6.0008,col="red")
+
