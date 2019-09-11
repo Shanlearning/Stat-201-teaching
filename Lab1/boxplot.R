ee4eece8de76876d180d2737635f4c1821bc0dc2
Shan Zhong <zhongshanyes@gmail.com>
2019-09-11 11:55:01 -0400

    Update boxplot.R

--- Lab1/boxplot.R
+++ Lab1/boxplot.R
@@ -1,6 +1,21 @@
 
+
 # load the data, chosse the data.csv you download
-dat <- read.csv(file.choose())
+dat <- read.csv("https://raw.githubusercontent.com/Shanlearning/Stat201/master/Lab1/data.csv")
+# a brief look at the data
+dat
+
+# draw boxplot for mean comparison
+boxplot(dat[,c("Random_sample_mean","Judgement_sample_mean")])
+# true population mean
+abline(h=5.5686)
+
+# draw boxplot for standard deviation comparison
+# plot column3 and column4
+boxplot(dat[,3:4])
+
+# true population standard deviation
+abline(h=6.0008,col="red")
 # a brief look at the data
 dat
 
