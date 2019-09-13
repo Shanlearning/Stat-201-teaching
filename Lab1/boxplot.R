86dadff1dea85124cfc72925025a421b1ea3cd51
Shan Zhong <zhongshanyes@gmail.com>
2019-09-13 11:48:15 -0400

    Update boxplot.R

--- Lab1/boxplot.R
+++ Lab1/boxplot.R
@@ -1,5 +1,6 @@
 dat <- read.csv("https://raw.githubusercontent.com/Shanlearning/Stat201/master/Lab1/Friday_class.csv")
 dat
+dat <- dat[-13,]
 
 boxplot(dat[,c("Random_sample_mean","Judgement_sample_mean")])
 abline(h=5.5686,col="red")
