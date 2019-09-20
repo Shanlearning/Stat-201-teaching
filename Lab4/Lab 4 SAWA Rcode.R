78ac08ff1e7987d111ec3e85979e9fa610771e92
Shan Zhong <zhongshanyes@gmail.com>
2019-09-20 04:00:26 -0400

    Shan

--- Lab4/Lab 4 SAWA Rcode.R
+++ Lab4/Lab 4 SAWA Rcode.R
@@ -11,16 +11,19 @@ Guessed_Distance = c(42,
       100
 )
 
+Guessed_Distance_11th_Object = 120
+
+#################################################################################
 True_Distance = c(39,
-      27,
-      16,
-      83,
-      130,
-      255,
-      8,
-      43,
-      94,
-      186
+                  27,
+                  16,
+                  83,
+                  130,
+                  255,
+                  8,
+                  43,
+                  94,
+                  186
 )
 
 Object = c( "Distance to Maps Box",
@@ -59,6 +62,7 @@ colnames(pred_dat) = c("Guessed_Distance","Guessed_Distance_square")
 fitted_linear = predict(linear_model,newdata = pred_dat)
 fitted_quadratic = predict(quadratic_model,newdata = pred_dat)
 
+par(mfrow=c(1,3))
 plot(True_Distance ~ Guessed_Distance, data = dat,pch=3,lwd=2)
 with(pred_dat, lines(x = Guessed_Distance, y = fitted_linear,col="red",lwd= 2))
 with(pred_dat, lines(x = Guessed_Distance, y = fitted_quadratic,col="blue",lwd= 2, lty=2))
@@ -66,5 +70,25 @@ text(x=Guessed_Distance,y=True_Distance,labels = rownames(dat),col ="green")
 legend("topleft", legend=c("Linear", "Quadratic"),
        col=c("red", "blue"), lty=1:2, cex=0.8)
 
+
+sd_linear = sqrt(sum(linear_model$residuals^2)/8)
+sd_quadriac = sqrt(sum(quadratic_model$residuals^2)/7)
+
+plot(True_Distance ~ Guessed_Distance, data = dat,pch=3,lwd=2,col="white")
+with(pred_dat, lines(x = Guessed_Distance, y = fitted_linear,col="red",lwd= 2))
+points(x=Guessed_Distance_11th_Object,y=100,pch=2,lwd=2,col="black")
+legend("topleft",legend="11th Object",col="black",pch=2)
+with(pred_dat, lines(x = Guessed_Distance, y = fitted_linear-2*sd_linear,col="pink",lwd= 2,lty=3))
+with(pred_dat, lines(x = Guessed_Distance, y = fitted_linear+2*sd_linear,col="pink",lwd= 2,lty=3))
+
+plot(True_Distance ~ Guessed_Distance, data = dat,pch=3,lwd=2,col="white")
+points(x=Guessed_Distance_11th_Object,y=100,pch=2,lwd=2,col="black")
+legend("topleft",legend="11th Object",col="black",pch=2)
+with(pred_dat, lines(x = Guessed_Distance, y = fitted_quadratic,col="blue",lwd= 2, lty=2))
+with(pred_dat, lines(x = Guessed_Distance, y = fitted_quadratic-2*sd_linear,col="green",lwd= 2,lty=3))
+with(pred_dat, lines(x = Guessed_Distance, y = fitted_quadratic+2*sd_linear,col="green",lwd= 2,lty=3))
+
 summary(linear_model)
 summary(quadratic_model)
+
+
